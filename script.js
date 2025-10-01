0245b4cfb352883349e508676edb32abbc3585bf
Saurabh singh <ajinkyabisht75@gmail.com>
2025-10-01 10:44:15 +0530

    adding record slider

--- script.js
+++ script.js
@@ -4,6 +4,72 @@ const Sideslider = document.getElementById('side-slider');
 Sideslider.classList.toggle('active');
 }
 
+// script for record slider 
+const slider = document.querySelector(".slider");
+const slideimage = document.querySelectorAll(".img");
+
+let slidecounter = 1;
+let length = slideimage.length;
+
+// to creating a button div 
+const control = document.querySelector('.control');
+for(let i = 0; i < length; i++){
+    const div = document.createElement("div");
+    div.className = "button";
+    control.appendChild(div);
+}
+
+// reset the background color of div buttons
+const buttons = document.querySelectorAll(".button");
+function resetbg(){
+    buttons.forEach((button) =>{
+        button.style.background = "transparent" ; 
+        button.addEventListener("mouseover", stopslideshow);
+        button.addEventListener("mouseout", startslideshow); 
+    });
+}
+
+buttons.forEach((button, i) => {
+    button.addEventListener("click", ()=>{
+        resetbg();
+        slider.style.transform = `translateX(-${i * 100}%)`;
+        slidecounter = i + 1;
+        buttons[i].style.background = 'black';
+    });
+});
+// changing colors while changing slides
+const changecolor = () =>{
+    resetbg()
+    buttons[slidecounter-1].style.background = "black";
+}
+// function for nextslide
+function nextslide(){
+    slider.style.transform = `translateX(-${slidecounter * 100}%)`;
+    slidecounter++;
+}
+// for firstslide
+function firstslide(){
+    slider.style.transform = `translateX(0px)`;
+    slidecounter = 1;
+}
+// function for automation
+let slideInterval;
+
+const startslideshow= ()=>{
+    slideInterval = setInterval(() =>{
+        slidecounter < length ? nextslide() : firstslide();
+        changecolor();
+    }, 2000);
+};
+startslideshow();
+
+const stopslideshow = () =>{
+    clearInterval(slideInterval);
+}
+slider.addEventListener("mouseover", stopslideshow);
+slider.addEventListener("mouseout", startslideshow);
+// script of slider end
+
 //  Player Image Box Toggle
 const playerimgBox = document.querySelectorAll('.players-imgbox');
 playerimgBox.forEach(box => {
